d90da3acf7e65dba035e3544cf52afc893bac913
chukreevAV <User@WIN-0UDOESQ8HEN>
2023-03-17 18:44:30 +0500

    Добавил проект тестов

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WalletsService.Models;
+
+namespace TestProject1
+{
+    [TestClass] public class UnitTest1
+    {
+        [TestMethod] public void TestWalletsContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WalletsContext>();
+            optionsBuilder.UseInMemoryDatabase("Wallets");
+            using var context = new WalletsContext(optionsBuilder.Options);
+            var count1 = context.Wallets.Count();
+            Assert.IsTrue(count1 == 0);
+        }
+    }
+}
--- WalletsService/Program.cs
+++ WalletsService/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WalletsService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContextFactory<WalletsContext>(opt
+    => opt.UseInMemoryDatabase("Wallets"));
 
 var app = builder.Build();
 
